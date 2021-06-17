d049f9c7d73e17baca0cb21d6b159f70c6ebcf80
Lyubomir Nanev <lyubomir@a-sharp.eu>
2021-06-17 11:29:24 +0300

    added CORS

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*')
+	res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
+	next()
+})
+
 const coinMarketCapRouter = require('./routes/coinMarketCap')
 app.use('/coin-market-cap', coinMarketCapRouter)
 const contractRouter = require('./routes/contract')
